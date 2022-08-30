935b8634935b8d309248ccbc91a001b64269dad6
manoubiovic <moetez12345@gmail.com>
2022-08-30 16:58:14 +0100

    first commit

---
+++ src/components/ListsMovies.js
@@ -0,0 +1,32 @@
+export const ListsMovies = [
+  {
+    id: Math.random(),
+    title: "Wicker Park",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/51hHoo8F9cL._AC_.jpg",
+    rate: 5,
+    trailler: "https://www.youtube.com/embed/Dy99aOBE2RU",
+  },
+  {
+    id: Math.random(),
+    title: "Inception",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/81p%2Bxe8cbnL._SY445_.jpg",
+    rate: 4,
+    trailler: "https://www.youtube.com/embed/8hP9D6kZseM",
+  },
+  {
+    id: Math.random(),
+    title: "Mad max",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/519QKrYfV1L._SX342_.jpg",
+    rate: 2,
+    trailler: "https://www.youtube.com/embed/hEJnMQG9ev8",
+  },
+];
